e66ab2f516cf53bbbc152cc9b679b1da9933f886
LAMZAOO <nhtaku07jank@icloud.com>
2022-02-20 21:24:46 +0900

    authentication

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 const express = require('express');
-const path =require('path');
+const path = require('path');
 const mongoose = require('mongoose');
-const ejsMate =require('ejs-mate');
+const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
-
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
+const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
-.then(() => {
-    console.log('MongoDBコネクションOK!!');
-})
-.catch(err => {
-    console.log('MongoDBコネクションエラー!!');
-    console.log(err);
-});
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => {
+        console.log('MongoDBコネクションOK！！');
+    })
+    .catch(err => {
+        console.log('MongoDBコネクションエラー！！！');
+        console.log(err);
+    });
 
 const app = express();
 
@@ -33,9 +35,9 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 const sessionConfig = {
     secret: 'mysecret',
@@ -43,24 +45,32 @@ const sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 *7, // cookieの有効期限(単位:ms)
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 };
-
 app.use(session(sessionConfig));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(flash());
 
 app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
-})
+});
 
 app.get('/', (req, res) => {
     res.render('home');
-    // ホーム
 });
 
+app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 
@@ -71,12 +81,11 @@ app.all('*', (req, res, next) => {
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) {
-        err.message = '問題が発生しました!!!';
+        err.message = '問題が起きました'
     }
     res.status(statusCode).render('error', { err });
-    // エラーハンドリング
 });
 
 app.listen(3000, () => {
-    console.log('ポート3000でリクエスト待ち受け中');
+    console.log('ポート3000でリクエスト待受中...');
 });
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const { campgroundSchema } = require('../schemas');
+const { isLoggedIn } = require('../middleware');
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
@@ -18,55 +19,50 @@ const validateCampground = (req, res, next) => {
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-    // キャンプ場一覧
 }));
 
-router.get('/new', catchAsync(async (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
-}));
+});
 
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
     if (!campground) {
-        req.flash('error', 'キャンプ場は見つかりませんでした!!!');
+        req.flash('error', 'キャンプ場は見つかりませんでした');
         return res.redirect('/campgrounds');
     }
     res.render('campgrounds/show', { campground });
-    // キャンプ場詳細
 }));
 
-router.post('/', validateCampground, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
+    // if (!req.body.campground) throw new ExpressError('不正なキャンプ場のデータです', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
-    req.flash('success', '新しいキャンプ場を登録しました!!!');
+    req.flash('success', '新しいキャンプ場を登録しました');
     res.redirect(`/campgrounds/${campground._id}`);
-    // キャンプ場の新規登録
 }));
 
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
-        req.flash('error', 'キャンプ場は見つかりませんでした!!!');
+        req.flash('error', 'キャンプ場は見つかりませんでした');
         return res.redirect('/campgrounds');
     }
     res.render('campgrounds/edit', { campground });
-    // キャンプ場の編集
 }));
 
-router.put('/:id',validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'キャンプ場を更新しました!!!');
+    req.flash('success', 'キャンプ場を更新しました');
     res.redirect(`/campgrounds/${campground._id}`);
-    // キャンプ場の更新
 }));
 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-    req.flash('success', 'キャンプ場を削除しました!!!');
+    req.flash('success', 'キャンプ場を削除しました');
     res.redirect('/campgrounds');
-    // キャンプ場の削除
 }));
 
 module.exports = router;
